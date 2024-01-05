2e1703472b51d7cbe50294f5d7937e046e7332d2
Prema-931 <premaskht@gmail.com>
2024-01-05 15:17:49 +0530

    locals

--- locals/variables.tf
+++ locals/variables.tf
@@ -1,9 +1,9 @@
-variable "instance_names" {
-    type = list  
-    default = ["mongodb","redis","mysql","rabbitmq","catalogue","payment","shipping","user","cart","web","dispatch"]
-
-    
+variable "isProd" {
+    type = bool
+    default = "false"
 }
+    
+
 variable "ami_id" {
     type = string
     default = "ami-03265a0778a880afb"
